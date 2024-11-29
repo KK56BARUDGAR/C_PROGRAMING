00459be3e4b3256158ed602430d50326da740c21
KK56BARUDGAR <kaifbarudgar6@gmail.com>
2024-11-29 22:29:37 +0530

    Update task1.c

--- EXPT1/task one.c
+++ EXPT1/task one.c
@@ -1,13 +1,31 @@
-#include<stdio.h>
-
+/* WAP to print your name, age, class, division and UIN on the screen. Use
+ escape sequences to properly format the output.
+ NAME: kaif barudgar
+ Roll.no:47
+ div:E
+ class: ECS
+ UIN: 241S063
+ */
+ #include<stdio.h>
+ 
  int main()
-{
+ {
  printf("Name\t: kaif barudgar\n");
  printf("roll.no\t: 47\n");
  printf("div\t: E\n");
  printf("age\t: 19 \n");
  printf("UIN\t: 241S063\n");
  printf("class\t: ECS\n");
-
+ 
  return 0;
  }
+ 
+ /* output:-
+ Name : kaif barudgar
+ roll.no : 47
+ div : E
+ age : 19
+ UIN : 241S063
+ class : ECS
+*/
+ 
