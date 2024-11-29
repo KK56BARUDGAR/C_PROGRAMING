3ba4500d4844a1e52480b4665caed6dd555b9b15
KK56BARUDGAR <kaifbarudgar6@gmail.com>
2024-11-29 10:28:24 +0530

    Create task2.c

---
+++ EXPT2/task2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int start_num, end_num, sum = 0, i;
+
+    printf("\t\t\t Sum Of Odd Numbers \n\n\n");
+    printf("\t\t\t By kaif barudgar \n\n");
+    printf("Enter a Starting Number: ");
+    scanf("%d", &start_num);
+    printf("Enter an Ending Number: ");
+    scanf("%d", &end_num);
+
+   
+    if (start_num > end_num) {
+        printf("Error: Starting Number is higher than Ending Number\n");
+        return 1;
+    }
+
+    
+    for (i = start_num; i <= end_num; i++) {
+        if (i % 2 != 0) { // Check if the number is odd
+            sum += i; // Add odd number to sum
+        }
+    }
+
+    printf("Sum of Odd Numbers Between %d and %d is %d\n", start_num, end_num, sum);
+
+    return 0;
+}
