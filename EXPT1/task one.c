e4ff9e5a56ae62cae7d66343a5bb39a2a93bf951
KK56BARUDGAR <kaifbarudgar6@gmail.com>
2024-11-29 10:14:19 +0530

    Create task one.c

---
+++ EXPT1/task one.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+ int main()
+{
+ printf("Name\t: kaif barudgar\n");
+ printf("roll.no\t: 47\n");
+ printf("div\t: E\n");
+ printf("age\t: 19 \n");
+ printf("UIN\t: 241S063\n");
+ printf("class\t: ECS\n");
+
+ return 0;
+ }
