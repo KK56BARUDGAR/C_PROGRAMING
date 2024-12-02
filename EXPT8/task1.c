bfaac0d640b378de4d74612a49198db66c3f63b1
KK56BARUDGAR <kaifbarudgar6@gmail.com>
2024-12-02 18:53:45 +0530

    Update task1.c

--- EXPT8/task1.c
+++ EXPT8/task1.c
@@ -1,3 +1,11 @@
+ /*
+ EXP NO : 8.1
+ TITLE : WAP TO FIND the length of a string without using library function
+ NAME : kaif barudgar
+ UIN :241S063
+ Roll.No:47
+ CLASS & DIV :F.E -ECS ENG(E3) */
+
 #include <stdio.h>
 int main() {
  char str[100];
@@ -9,3 +17,9 @@ int main() {
  }
  printf("The length of the string is: %d\n", length);
  return 0;
+}
+/*
+OUTPUT :
+Enter a string: hello world
+The length of the string is: 5
+*/
