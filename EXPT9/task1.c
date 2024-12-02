f69289c69800083f1d612b4c29d2a171c7c7987d
KK56BARUDGAR <kaifbarudgar6@gmail.com>
2024-12-02 18:56:25 +0530

    Update task1.c

--- EXPT9/task1.c
+++ EXPT9/task1.c
@@ -1,5 +1,11 @@
-//Designing a structure student_record to contain name, roll_number, and total marks obtained. 
-//Write a program to read 5 students data from the user and then display the topper on the screen.
+/*
+EXP NO : 9.1
+ TITLE : design a structure record to contain name , roll_number , and total marks obtained. write a program to read 5 student data from the user and then display the topper on the screen
+ NAME : kaif barudgar
+ UIN :241S063
+ Roll.No:47
+ CLASS & DIV :F.E -ECS ENG(E3) */
+
 #include <stdio.h>
 #include <string.h>
 struct student_record {
@@ -28,3 +34,29 @@ int main() {
  printf("Total Marks: %.2f\n", students[topperIndex].total_marks);
  return 0;
 }
+
+/*
+OUTPUT :Enter details for student 1:
+Name: pratik
+Roll Number: 73
+Total Marks: 100
+Enter details for student 2:
+Name: abhay
+Roll Number: 88
+Total Marks: 99
+Enter details for student 3:
+Name: arman
+Roll Number: 84
+Total Marks: 90
+Enter details for student 4:
+Name: shubham
+Roll Number: 55
+Total Marks: 98
+Enter details for student 5:
+Name: ganpat
+Roll Number: 66
+Total Marks: 97
+Topper: pratik
+Roll Number: 73
+Total Marks: 100
+*/
